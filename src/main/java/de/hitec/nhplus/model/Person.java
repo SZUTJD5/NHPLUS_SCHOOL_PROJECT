95d7946ab686ac2c4b75fbe5f6685bc1333bc025
rajbirsingh259 <rajbir2@schule.bremen.de>
2024-04-25 13:32:42 +0200

    Added documentation for the Person.java code

--- src/main/java/de/hitec/nhplus/model/Person.java
+++ src/main/java/de/hitec/nhplus/model/Person.java
@@ -1,36 +1,48 @@
+// Paketanweisung und Importe
 package de.hitec.nhplus.model;
 
 import javafx.beans.property.SimpleStringProperty;
 
+// Abstrakte Klasse Person
 public abstract class Person {
-    private final SimpleStringProperty firstName;
-    private final SimpleStringProperty surname;
 
+    // Eigenschaften der Person als SimpleStringProperty
+    private final SimpleStringProperty firstName; // Vorname
+    private final SimpleStringProperty surname; // Nachname
+
+    // Konstruktor der Klasse Person
     public Person(String firstName, String surname) {
+        // Initialisierung der Vorname- und Nachname-Eigenschaften mit den übergebenen Werten
         this.firstName = new SimpleStringProperty(firstName);
         this.surname = new SimpleStringProperty(surname);
     }
 
+    // Getter für den Vorname
     public String getFirstName() {
         return firstName.get();
     }
 
+    // Getter für den Vorname als SimpleStringProperty
     public SimpleStringProperty firstNameProperty() {
         return firstName;
     }
 
+    // Setter für den Vorname
     public void setFirstName(String firstName) {
         this.firstName.set(firstName);
     }
 
+    // Getter für den Nachname
     public String getSurname() {
         return surname.get();
     }
 
+    // Getter für den Nachname als SimpleStringProperty
     public SimpleStringProperty surnameProperty() {
         return surname;
     }
 
+    // Setter für den Nachname
     public void setSurname(String surname) {
         this.surname.set(surname);
     }
