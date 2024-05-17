7ffbd6f90a35907ff6df09357e5e3520a8c7d45f
SZUTJD5 <167878340+SZUTJD5@users.noreply.github.com>
2024-05-17 15:21:34 +0200

    Made Login compare passwords with usernames and passwords from the login table and then return the login_ID for later use

--- src/main/java/de/hitec/nhplus/controller/LoginController.java
+++ src/main/java/de/hitec/nhplus/controller/LoginController.java
@@ -1,44 +1,87 @@
 package de.hitec.nhplus.controller;
 
 import de.hitec.nhplus.Main;
+import de.hitec.nhplus.datastorage.ConnectionBuilder;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginController {
-    // Feld zum eintragen des Nutzernamen
     public TextField textFieldLoginName;
-    // Feld zum eintragen des Passworts
     public TextField textFieldPassword;
     public BorderPane loginBorderPane;
     public Button buttonLogin;
-    // Button zum Login
+    public int loggedInCaregiver;
+
+    private static void alert(Exception e, String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(message);
+        alert.setContentText(e.getMessage());
+        alert.showAndWait();
+    }
 
     @FXML
     private void handleLogin() {
-        String password = "adminadmin";
-        String loginName = "Jhon";
-        if (textFieldLoginName.getText().equals(loginName) && textFieldPassword.getText().equals(password)) {
-            try {
-                FXMLLoader loader = new FXMLLoader(Main.class.getResource("/de/hitec/nhplus/MainWindowView.fxml"));
-                BorderPane mainBorderPane = loader.load();
+        final String loginName = textFieldLoginName.getText();
+        final String password = textFieldPassword.getText();
 
-                // Get the stage and adjust its size to fit the content
-                Stage stage = (Stage) loginBorderPane.getScene().getWindow();
-                Scene scene = new Scene(mainBorderPane);
-                stage.setScene(scene);
-                stage.sizeToScene();
-            } catch (IOException exception) {
-                System.setErr(System.err);
+        if (loginName.isEmpty() || password.isEmpty()) {
+            alert(new Exception(), "Nutzername oder Password fehlt!", "Bitte gebe einen Benutzernamen und Password ein!");
+            return;
+        }
+
+        final String SQL = "SELECT login_ID FROM logins WHERE name = ? AND password = ?";
+        try (Connection connection = ConnectionBuilder.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
+
+            if (connection.isClosed()) {
+                alert(new SQLException("Failed to establish a database connection."), "Fehler!", "Konnte keine Verbindung zur Datenbank aufbauen!");
             }
+
+            preparedStatement.setString(1, loginName);
+            preparedStatement.setString(2, password);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    loggedInCaregiver = resultSet.getInt("login_ID");
+                    System.out.println(loggedInCaregiver);
+                    // Proceed with the successful login process, e.g., load the main window
+                    loadMainWindow();
+                } else {
+                    alert(new Exception(), "Login fehlgeschlagen", "Ungültiger Nutzername oder Passwort!");
+                    ConnectionBuilder.closeConnection();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            alert(e, "Datenbankfehler", "Ein Fehler ist bei der Datenbankabfrage aufgetreten.");
         }
     }
 
+    private void loadMainWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/de/hitec/nhplus/MainWindowView.fxml"));
+            BorderPane mainBorderPane = loader.load();
 
+            // Get the stage and adjust its size to fit the content
+            Stage stage = (Stage) loginBorderPane.getScene().getWindow();
+            Scene scene = new Scene(mainBorderPane);
+            stage.setScene(scene);
+            stage.sizeToScene();
+            ConnectionBuilder.closeConnection();
+        } catch (IOException e) {
+            alert(e, "Datenbankfehler", "Ein Fehler ist beim Laden des Hauptfensters aufgetreten.");
+        }
+    }
 }
--- src/main/java/de/hitec/nhplus/datastorage/PatientDao.java
+++ src/main/java/de/hitec/nhplus/datastorage/PatientDao.java
@@ -1,176 +1,176 @@
-package de.hitec.nhplus.datastorage;
+    package de.hitec.nhplus.datastorage;
 
-import de.hitec.nhplus.model.Patient;
-import de.hitec.nhplus.utils.DateConverter;
+    import de.hitec.nhplus.model.Patient;
+    import de.hitec.nhplus.utils.DateConverter;
 
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-
-/**
- * Implementiert die Schnittstelle <code>DaoImp</code>. Überschreibt Methoden, um bestimmte <code>PreparedStatements</code> zu generieren.
- * um die spezifischen SQL-Anweisungen auszuführen.
- */
-public class PatientDao extends DaoImp<Patient> {
+    import java.sql.*;
+    import java.time.LocalDate;
+    import java.util.ArrayList;
 
     /**
-     * Der Konstruktor initiiert ein Objekt von <code>PatientDao</code> und übergibt die Verbindung an seine Superklasse.
-     *
-     * @param connection Objekt von <code>Connection</code> zur Ausführung der SQL-Anweisungen.
+     * Implementiert die Schnittstelle <code>DaoImp</code>. Überschreibt Methoden, um bestimmte <code>PreparedStatements</code> zu generieren.
+     * um die spezifischen SQL-Anweisungen auszuführen.
      */
-    public PatientDao(Connection connection) {
-        super(connection);
-    }
+    public class PatientDao extends DaoImp<Patient> {
 
-    /**
-     * Erzeugt ein <code>PreparedStatement</code>, um das angegebene Objekt von <code>Patient</code> beizubehalten.
-     *
-     * @param patient Objekt von <code>Patient</code>, das bestehen bleiben soll.
-     * @return <code>PreparedStatement</code> zum Einfügen des angegebenen Patienten.
-     */
-    @Override
-    protected PreparedStatement getCreateStatement(Patient patient) {
-        PreparedStatement preparedStatement = null;
-        try {
-            final String SQL = "INSERT INTO patient (firstname, surname, dateOfBirth, carelevel, roomnumber, locked) " + "VALUES (?, ?, ?, ?, ?, ?)";
-            preparedStatement = this.connection.prepareStatement(SQL);
-            preparedStatement.setString(1, patient.getFirstName());
-            preparedStatement.setString(2, patient.getSurname());
-            preparedStatement.setString(3, patient.getDateOfBirth());
-            preparedStatement.setString(4, patient.getCareLevel());
-            preparedStatement.setString(5, patient.getRoomNumber());
-            preparedStatement.setString(6, patient.getLocked() ? "1" : "0"); // Set default value for locked
-        } catch (SQLException exception) {
-            System.setErr(System.err);
+        /**
+         * Der Konstruktor initiiert ein Objekt von <code>PatientDao</code> und übergibt die Verbindung an seine Superklasse.
+         *
+         * @param connection Objekt von <code>Connection</code> zur Ausführung der SQL-Anweisungen.
+         */
+        public PatientDao(Connection connection) {
+            super(connection);
         }
-        return preparedStatement;
-    }
 
-    /**
-     * Erzeugt ein <code>PreparedStatement</code>, um einen Patienten anhand einer bestimmten Patienten-ID (PID) abzufragen.
-     *
-     * @param pid Patienten-ID zur Abfrage.
-     * @return <code>PreparedStatement</code> zur Abfrage des Patienten.
-     */
-    @Override
-    protected PreparedStatement getReadByIDStatement(long pid) {
-        PreparedStatement preparedStatement = null;
-        try {
-            final String SQL = "SELECT * FROM patient WHERE pid = ? AND locked = ?";
-            preparedStatement = this.connection.prepareStatement(SQL);
-            preparedStatement.setLong(1, pid);
-            preparedStatement.setBoolean(2, false); // Nur Patienten anzeigen die nicht gesperrt sind.
-        } catch (SQLException exception) {
-            System.setErr(System.err);
+        /**
+         * Erzeugt ein <code>PreparedStatement</code>, um das angegebene Objekt von <code>Patient</code> beizubehalten.
+         *
+         * @param patient Objekt von <code>Patient</code>, das bestehen bleiben soll.
+         * @return <code>PreparedStatement</code> zum Einfügen des angegebenen Patienten.
+         */
+        @Override
+        protected PreparedStatement getCreateStatement(Patient patient) {
+            PreparedStatement preparedStatement = null;
+            try {
+                final String SQL = "INSERT INTO patient (firstname, surname, dateOfBirth, carelevel, roomnumber, locked) " + "VALUES (?, ?, ?, ?, ?, ?)";
+                preparedStatement = this.connection.prepareStatement(SQL);
+                preparedStatement.setString(1, patient.getFirstName());
+                preparedStatement.setString(2, patient.getSurname());
+                preparedStatement.setString(3, patient.getDateOfBirth());
+                preparedStatement.setString(4, patient.getCareLevel());
+                preparedStatement.setString(5, patient.getRoomNumber());
+                preparedStatement.setString(6, patient.getLocked() ? "1" : "0"); // Set default value for locked
+            } catch (SQLException exception) {
+                System.setErr(System.err);
+            }
+            return preparedStatement;
         }
-        return preparedStatement;
-    }
 
-    /**
-     * Ordnet ein <code>ResultSet</code> eines Patienten einem Objekt von <code>Patient</code> zu.
-     *
-     * @param result ResultSet mit einer einzelnen Zeile. Spalten werden einem Objekt der Klasse <code>Patient</code> zugeordnet.
-     * @return Objekt der Klasse <code>Patient</code> mit den Daten aus dem resultSet.
-     */
-    @Override
-    protected Patient getInstanceFromResultSet(ResultSet result) throws SQLException {
-        return new Patient(result.getInt(1), result.getString(2), result.getString(3), DateConverter.convertStringToLocalDate(result.getString(4)), result.getString(5), result.getString(6), result.getBoolean(7));
-    }
-
-    /**
-     * Erzeugt ein <code>PreparedStatement</code> zur Abfrage aller Patienten.
-     *
-     * @return <code>PreparedStatement</code> zur Abfrage aller Patienten.
-     */
-    @Override
-    protected PreparedStatement getReadAllStatement() {
-        PreparedStatement statement = null;
-        try {
-            final String SQL = "SELECT * FROM patient WHERE locked = ?";
-            statement = this.connection.prepareStatement(SQL);
-            statement.setBoolean(1, false); // Nur Patienten anzeigen die nicht gesperrt sind.
-        } catch (SQLException exception) {
-            System.setErr(System.err);
+        /**
+         * Erzeugt ein <code>PreparedStatement</code>, um einen Patienten anhand einer bestimmten Patienten-ID (PID) abzufragen.
+         *
+         * @param pid Patienten-ID zur Abfrage.
+         * @return <code>PreparedStatement</code> zur Abfrage des Patienten.
+         */
+        @Override
+        protected PreparedStatement getReadByIDStatement(long pid) {
+            PreparedStatement preparedStatement = null;
+            try {
+                final String SQL = "SELECT * FROM patient WHERE pid = ? AND locked = ?";
+                preparedStatement = this.connection.prepareStatement(SQL);
+                preparedStatement.setLong(1, pid);
+                preparedStatement.setBoolean(2, false); // Nur Patienten anzeigen die nicht gesperrt sind.
+            } catch (SQLException exception) {
+                System.setErr(System.err);
+            }
+            return preparedStatement;
         }
-        return statement;
-    }
 
-    /**
-     * Ordnet ein <code>ResultSet</code> aller Patienten einer <code>ArrayList</code> von <code>Patient</code>-Objekten zu.
-     *
-     * @param result ResultSet mit allen Zeilen. Die Spalten werden Objekten der Klasse <code>Patient</code> zugeordnet.
-     * @return <code>ArrayList</code> mit Objekten der Klasse <code>Patient</code> aller Zeilen im
-     * <code>ResultSet</code>.
-     */
-    @Override
-    protected ArrayList<Patient> getListFromResultSet(ResultSet result) throws SQLException {
-        ArrayList<Patient> list = new ArrayList<>();
-        while (result.next()) {
-            LocalDate date = DateConverter.convertStringToLocalDate(result.getString(4));
-            Patient patient = new Patient(result.getInt(1), result.getString(2), result.getString(3), date, result.getString(5), result.getString(6), result.getBoolean(7));
-            list.add(patient);
+        /**
+         * Ordnet ein <code>ResultSet</code> eines Patienten einem Objekt von <code>Patient</code> zu.
+         *
+         * @param result ResultSet mit einer einzelnen Zeile. Spalten werden einem Objekt der Klasse <code>Patient</code> zugeordnet.
+         * @return Objekt der Klasse <code>Patient</code> mit den Daten aus dem resultSet.
+         */
+        @Override
+        protected Patient getInstanceFromResultSet(ResultSet result) throws SQLException {
+            return new Patient(result.getInt(1), result.getString(2), result.getString(3), DateConverter.convertStringToLocalDate(result.getString(4)), result.getString(5), result.getString(6), result.getBoolean(7));
         }
-        return list;
-    }
 
-    /**
-     * Erzeugt ein <code>PreparedStatement</code>, um den angegebenen identifizierten Patienten zu aktualisieren
-     * nach der ID des Patienten (pid).
-     *
-     * @param patient Patientenobjekt, das aktualisiert werden soll.
-     * @return <code>PreparedStatement</code>, um den angegebenen Patienten zu aktualisieren.
-     */
-    @Override
-    protected PreparedStatement getUpdateStatement(Patient patient) {
-        PreparedStatement preparedStatement = null;
-        try {
-            final String SQL = "UPDATE patient SET " + "firstname = ?, " + "surname = ?, " + "dateOfBirth = ?, " + "carelevel = ?, " + "roomnumber = ? " + "WHERE pid = ?";
-            preparedStatement = this.connection.prepareStatement(SQL);
-            preparedStatement.setString(1, patient.getFirstName());
-            preparedStatement.setString(2, patient.getSurname());
-            preparedStatement.setString(3, patient.getDateOfBirth());
-            preparedStatement.setString(4, patient.getCareLevel());
-            preparedStatement.setString(5, patient.getRoomNumber());
-            preparedStatement.setLong(6, patient.getPid());
-        } catch (SQLException exception) {
-            System.setErr(System.err);
+        /**
+         * Erzeugt ein <code>PreparedStatement</code> zur Abfrage aller Patienten.
+         *
+         * @return <code>PreparedStatement</code> zur Abfrage aller Patienten.
+         */
+        @Override
+        protected PreparedStatement getReadAllStatement() {
+            PreparedStatement statement = null;
+            try {
+                final String SQL = "SELECT * FROM patient WHERE locked = ?";
+                statement = this.connection.prepareStatement(SQL);
+                statement.setBoolean(1, false); // Nur Patienten anzeigen die nicht gesperrt sind.
+            } catch (SQLException exception) {
+                System.setErr(System.err);
+            }
+            return statement;
         }
-        return preparedStatement;
-    }
 
-    /**
-     * Aktualisiert den Sperrstatus eines Patienten.
-     *
-     * @param pid    Die ID des Patienten.
-     * @param locked true, wenn der Patient gesperrt ist, andernfalls false.
-     * @throws SQLException, wenn ein Datenbankzugriffsfehler auftritt.
-     */
-    public void updateLockStatus(long pid, boolean locked) throws SQLException {
-        final String SQL = "UPDATE patient SET locked = ? WHERE pid = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
-            preparedStatement.setBoolean(1, locked); // Wechselt locked auf "1"
-            preparedStatement.setLong(2, pid);
-            preparedStatement.executeUpdate();
+        /**
+         * Ordnet ein <code>ResultSet</code> aller Patienten einer <code>ArrayList</code> von <code>Patient</code>-Objekten zu.
+         *
+         * @param result ResultSet mit allen Zeilen. Die Spalten werden Objekten der Klasse <code>Patient</code> zugeordnet.
+         * @return <code>ArrayList</code> mit Objekten der Klasse <code>Patient</code> aller Zeilen im
+         * <code>ResultSet</code>.
+         */
+        @Override
+        protected ArrayList<Patient> getListFromResultSet(ResultSet result) throws SQLException {
+            ArrayList<Patient> list = new ArrayList<>();
+            while (result.next()) {
+                LocalDate date = DateConverter.convertStringToLocalDate(result.getString(4));
+                Patient patient = new Patient(result.getInt(1), result.getString(2), result.getString(3), date, result.getString(5), result.getString(6), result.getBoolean(7));
+                list.add(patient);
+            }
+            return list;
+        }
+
+        /**
+         * Erzeugt ein <code>PreparedStatement</code>, um den angegebenen identifizierten Patienten zu aktualisieren
+         * nach der ID des Patienten (pid).
+         *
+         * @param patient Patientenobjekt, das aktualisiert werden soll.
+         * @return <code>PreparedStatement</code>, um den angegebenen Patienten zu aktualisieren.
+         */
+        @Override
+        protected PreparedStatement getUpdateStatement(Patient patient) {
+            PreparedStatement preparedStatement = null;
+            try {
+                final String SQL = "UPDATE patient SET " + "firstname = ?, " + "surname = ?, " + "dateOfBirth = ?, " + "carelevel = ?, " + "roomnumber = ? " + "WHERE pid = ?";
+                preparedStatement = this.connection.prepareStatement(SQL);
+                preparedStatement.setString(1, patient.getFirstName());
+                preparedStatement.setString(2, patient.getSurname());
+                preparedStatement.setString(3, patient.getDateOfBirth());
+                preparedStatement.setString(4, patient.getCareLevel());
+                preparedStatement.setString(5, patient.getRoomNumber());
+                preparedStatement.setLong(6, patient.getPid());
+            } catch (SQLException exception) {
+                System.setErr(System.err);
+            }
+            return preparedStatement;
+        }
+
+        /**
+         * Aktualisiert den Sperrstatus eines Patienten.
+         *
+         * @param pid    Die ID des Patienten.
+         * @param locked true, wenn der Patient gesperrt ist, andernfalls false.
+         * @throws SQLException, wenn ein Datenbankzugriffsfehler auftritt.
+         */
+        public void updateLockStatus(long pid, boolean locked) throws SQLException {
+            final String SQL = "UPDATE patient SET locked = ? WHERE pid = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
+                preparedStatement.setBoolean(1, locked); // Wechselt locked auf "1"
+                preparedStatement.setLong(2, pid);
+                preparedStatement.executeUpdate();
+            }
+        }
+
+
+        /**
+         * Erzeugt ein <code>PreparedStatement</code>, um einen Patienten mit der angegebenen ID zu löschen.
+         *
+         * @param pid-ID des zu löschenden Patienten.
+         * @return <code>PreparedStatement</code>, um den Patienten mit der angegebenen ID zu löschen.
+         */
+        @Override
+        protected PreparedStatement getDeleteStatement(long pid) {
+            PreparedStatement preparedStatement = null;
+            try {
+                final String SQL = "DELETE FROM patient WHERE pid = ?";
+                preparedStatement = this.connection.prepareStatement(SQL);
+                preparedStatement.setLong(1, pid);
+            } catch (SQLException exception) {
+                System.setErr(System.err);
+            }
+            return preparedStatement;
         }
     }
-
-
-    /**
-     * Erzeugt ein <code>PreparedStatement</code>, um einen Patienten mit der angegebenen ID zu löschen.
-     *
-     * @param pid-ID des zu löschenden Patienten.
-     * @return <code>PreparedStatement</code>, um den Patienten mit der angegebenen ID zu löschen.
-     */
-    @Override
-    protected PreparedStatement getDeleteStatement(long pid) {
-        PreparedStatement preparedStatement = null;
-        try {
-            final String SQL = "DELETE FROM patient WHERE pid = ?";
-            preparedStatement = this.connection.prepareStatement(SQL);
-            preparedStatement.setLong(1, pid);
-        } catch (SQLException exception) {
-            System.setErr(System.err);
-        }
-        return preparedStatement;
-    }
-}
